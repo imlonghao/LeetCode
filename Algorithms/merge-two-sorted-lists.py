3ca8b211e620ef7be945ac8cdcd75bb812eb955a
imlonghao <shield@fastmail.com>
2016-06-13 01:28:42 +0800

    Improve the Algorithm for Question 21

--- Algorithms/merge-two-sorted-lists.py
+++ Algorithms/merge-two-sorted-lists.py
@@ -13,36 +13,15 @@ class Solution(object):
         :rtype: ListNode
         """
 
-        def merge(l1, l2):
-            if l1 is None and l2 is not None:
-                root = ListNode(l2.val)
-                root.next = merge(None, l2.next)
-                return root
-            elif l1 is not None and l2 is None:
-                root = ListNode(l1.val)
-                root.next = merge(l1.next, None)
-                return root
-            elif l1 is None and l2 is None:
-                return
-            else:
-                if l1.val >= l2.val:
-                    root = ListNode(l2.val)
-                    if l2.next is None:
-                        root.next = merge(l1, None)
-                        return root
-                    if l1.val >= l2.next.val:
-                        root.next = merge(l1, l2.next)
-                    else:
-                        root.next = merge(l1, l2.next)
-                else:
-                    root = ListNode(l1.val)
-                    if l1.next is None:
-                        root.next = merge(None, l2)
-                        return root
-                    if l2.val >= l1.next.val:
-                        root.next = merge(l1.next, l2)
-                    else:
-                        root.next = merge(l1.next, l2)
-                return root
-
-        return merge(l1, l2)
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        root = None
+        if l1.val >= l2.val:
+            root = l2
+            root.next = self.mergeTwoLists(l1, l2.next)
+        else:
+            root = l1
+            root.next = self.mergeTwoLists(l1.next, l2)
+        return root
